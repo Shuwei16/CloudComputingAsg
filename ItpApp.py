684b51739ab45c513cffa94044319a84b10efb2b
Shuwei16 <wangsw-wm20@student.tarc.edu.my>
2023-09-18 22:37:45 +0800

    routing

--- ItpApp.py
+++ ItpApp.py
@@ -23,9 +23,95 @@ region = customregion
 # table = 'employee'
 
 
+# Main
 @app.route("/")
-def home():
+def index():
     return render_template('index.html')
+
+# Student
+@app.route("/student/login")
+def studentLogin():
+    return render_template('student/login.html')
+
+@app.route("/student/register")
+def studentRegister():
+    return render_template('student/register.html')
+
+@app.route("/student/registerSuccess")
+def studentRegisterSuccess():
+    return render_template('student/registerSuccess.html')
+
+@app.route("/student/home")
+def studentHome():
+    return render_template('student/home.html')
+
+@app.route("/student/companyList")
+def studentCompanyList():
+    return render_template('student/companyList.html')
+
+@app.route("/student/companyDetail")
+def studentCompanyDetail():
+    return render_template('student/companyDetail.html')
+
+@app.route("/student/applicationHistory")
+def studentApplicationHistory():
+    return render_template('student/applicationHistory.html')
+
+# Lecturer
+@app.route("/lecturer/login")
+def lecturerLogin():
+    return render_template('lecturer/login.html')
+
+@app.route("/lecturer/home")
+def lecturerHome():
+    return render_template('lecturer/home.html')
+
+@app.route("/lecturer/studentDetail")
+def lecturerDetail():
+    return render_template('lecturer/studentDetail.html')
+
+# Company
+@app.route("/company/login")
+def companyLogin():
+    return render_template('company/login.html')
+
+@app.route("/company/register")
+def companyRegister():
+    return render_template('company/register.html')
+
+@app.route("/company/registerSuccess")
+def companyRegisterSuccess():
+    return render_template('company/registerSuccess.html')
+
+@app.route("/company/home")
+def companyHome():
+    return render_template('company/home.html')
+
+@app.route("/company/studentApplication")
+def companyStudentApplication():
+    return render_template('company/studentApplication.html')
+
+# Admin
+@app.route("/admin/login")
+def adminLogin():
+    return render_template('admin/login.html')
+
+@app.route("/admin/home")
+def adminHome():
+    return render_template('admin/home.html')
+
+@app.route("/admin/companyList")
+def adminCompanyList():
+    return render_template('admin/companyList.html')
+
+@app.route("/admin/companyDetail")
+def adminCompanyDetail():
+    return render_template('admin/companyDetail.html')
+
+@app.route("/admin/lecturerList")
+def adminLecturerList():
+    return render_template('admin/lecturerList.html')
+
 
 
 # @app.route("/about", methods=['POST'])
